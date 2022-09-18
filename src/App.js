011f9491263638bf559b9799ed1d3886ff8ade71
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-18 11:56:34 +0600

    react-with-node

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const name= event.target.name.value;
+    const email = event.target.email.value;
+    const user=(name,email);
+
+    
+
+
+    //post data to server
+fetch('http://localhost:4000/user' , {
+  method:'POST',
+  header:{
+    "content-type" :"aplication/json",
+  },
+  body:JSON.stringify(user)
+})
+.then(res=>res.json())
+.then(data=>{
+  const newUser = [...users,data];
+  setUsers(newUser);
+  console.log(data);
+})
+
+    
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My own Data :{users.length}</h2>
+
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" name="name" id="" placeholder="name" />
+        <input type="text" name="email" placeholder="email" id="" />
+        <input type="submit" value="add user" />
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>id:{user.id},name:{user.name},email:{user.email}</li>
+        ))}
+      </ul>
     </div>
   );
 }
